429e70ed6f9203ae6993d7836af41819155d0937
Girish Kumar <52626613+GirishKumarSp@users.noreply.github.com>
2020-12-08 18:30:47 +0530

    Add files via upload

---
+++ Find ASCII Value of Character.py
@@ -0,0 +1,11 @@
+# Program to find the ASCII value of the given character
+
+c = 'p'
+print("The ASCII value of '" + c + "' is", ord(c))
+#The ord() function returns the number representing the unicode code of a specified character.
+
+"""
+Output
+
+The ASCII value of 'p' is 112
+"""
